8d2a4419ee6b53db49c44e8d73caaddb9d9857c5
Eason Chang <eason@easonchang.com>
2024-02-29 15:18:14 -0700

    Add background gradient for timezone display

--- src/components/TimezoneDisplay.tsx
+++ src/components/TimezoneDisplay.tsx
@@ -43,20 +43,22 @@ export function TimezoneDisplay({
       </div>
 
       <div className="overflow-x-hidden">
-        <div className="bg-gray-100 py-2 px-3 rounded-md w-full">
+        <div className="flex items-center h-[58px] rounded-[15px] w-full border border-[#DBDBDB] overflow-hidden">
           <div
-            className="flex items-center -mx-3"
+            className="flex items-center h-full"
             style={{
               transform: `translateX(calc(50vw - 16px - 24px - ${
                 (currentTimeInZone.hour + currentTimeInZone.minute / 60) * 32
-              }px - 768px))`,
+              }px - (32px * 24)))`,
+              background:
+                'linear-gradient(90deg, rgba(31,52,182,1) 0%, rgba(31,52,182,1) 8.333%, rgba(135,208,240,1) 11.111%, rgba(135,208,240,1) 23.611%, rgba(31,52,182,1) 26.388%, rgba(31,52,182,1) 41.666%, rgba(135,208,240,1) 44.444%, rgba(135,208,240,1) 56.944%, rgba(31,52,182,1) 59.722%, rgba(31,52,182,1) 75%, rgba(135,208,240,1) 77.777%, rgba(135,208,240,1) 90.277%, rgba(31,52,182,1) 93.055%, rgba(31,52,182,1) 100%)',
             }}
           >
-            {Array.from({ length: 160 }).map((_, i) => (
+            {Array.from({ length: 145 }).map((_, i) => (
               <div key={i} className="flex justify-end w-[16px] shrink-0">
                 <div
                   className={cn(
-                    'w-[2px] shrink-0 bg-gray-300',
+                    'w-[2px] shrink-0 bg-white',
                     i % 2 === 0 ? 'h-[18px]' : 'h-[9px]'
                   )}
                 />
@@ -69,10 +71,10 @@ export function TimezoneDisplay({
           style={{
             transform: `translateX(calc(50vw - 16px - 24px - ${
               (currentTimeInZone.hour + currentTimeInZone.minute / 60) * 32
-            }px - 768px))`,
+            }px - (32px * 24)))`,
           }}
         >
-          {Array.from({ length: 80 }).map((_, i) => (
+          {Array.from({ length: 73 }).map((_, i) => (
             <div className="w-[32px] shrink-0 text-center" key={i}>
               {pad(i % 24)}
             </div>
