cc55af2a146e28925fa4eb848473376aa086e44c
Eason Chang <eason@easonchang.com>
2024-03-01 21:33:58 -0700

    Update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
       <nav className="flex w-full items-center justify-between py-4 px-6 border-b border-gray-200">
         <h1 className="text-xl font-bold">Time Zone Converter</h1>
         <div className="flex items-center space-x-4">
-          <Button variant="link" asChild>
+          <Button variant="link" asChild className="text-foreground">
             <Link href="/about">About</Link>
           </Button>
           <DarkModeToggle />
@@ -120,6 +120,7 @@ export default function Home() {
               onClick={() => {
                 setIsEditing((isEditing) => !isEditing);
               }}
+              className="px-5 py-2"
             >
               {isEditing ? 'Done' : 'Edit'}
             </Button>
@@ -127,6 +128,7 @@ export default function Home() {
               onClick={() => {
                 setOpenTimezonePickerDialog(true);
               }}
+              className="px-5 py-2"
             >
               Add
             </Button>
