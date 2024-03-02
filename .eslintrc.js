d74e6522f3e0a707ec1c6583e471ac52932cb2a4
Eason Chang <eason@easonchang.com>
2024-03-01 21:19:44 -0700

    Update eslintrc for storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   overrides: [
     {
       files: '**/*.{ts,tsx}',
-      extends: ['next/core-web-vitals', 'eason/typescript'],
+      extends: [
+        'next/core-web-vitals',
+        'eason/typescript',
+        'plugin:storybook/recommended',
+      ],
       rules: {
         // 'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': [
