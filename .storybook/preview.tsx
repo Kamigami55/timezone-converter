dd8bcc3c0886c78c89a6c9d508749b536ea9425f
Eason Chang <eason@easonchang.com>
2024-03-09 16:38:54 -0700

    Import next inter font into storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,9 @@
+import React from 'react';
+
 import type { Preview } from '@storybook/react';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +14,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <main className={inter.className}>
+        <Story />
+      </main>
+    ),
+  ],
 };
 
 export default preview;
